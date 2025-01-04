5e1cfd3b26a3333ecda7e21b484feb679fcbde26
alibahadirabra <alibahadir.kus@abra.partners>
2025-01-04 19:58:26 +0300

    a

--- traquent/public/js/traquent/ui/theme_switcher.js
+++ traquent/public/js/traquent/ui/theme_switcher.js
@@ -64,11 +64,11 @@ traquent.ui.ThemeSwitcher = class ThemeSwitcher {
 				// 	label: __("Timeless Night"),
 				// 	info: __("Dark Theme"),
 				// },  ali bahadır kuş kaldırdı
-				{
-					name: "automatic",
-					label: __("Automatic"),
-					info: __("Uses system's theme to switch between light and dark mode"),
-				},
+				// {
+				// 	name: "automatic",
+				// 	label: __("Automatic"),
+				// 	info: __("Uses system's theme to switch between light and dark mode"),
+				// },
 			];
 
 			resolve(this.themes);

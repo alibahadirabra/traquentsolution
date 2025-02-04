469810eec63b35cd1519a348c0b2f4db6a5c554d
alibahadirabra <alibahadir.kus@abra.partners>
2025-02-04 10:57:15 +0300

    a

--- traquent/api/getCustomer.py
+++ traquent/api/getCustomer.py
@@ -5,9 +5,75 @@ from traquent import _
 def get_filtered_records( filters=None, fields=None):
  
     filters = filters or {}
-    fields = fields or ["customer_name","customer_surname","customer_type","customer_group","lead_source","territory","customer_owner","referance_customer","birth_date","tckn","contacs_email","contacs_phone","website","phone","phone_ext"]
+    fields = fields or [                
+    "customer_type" ,                             #Müşteri Tipi
+# Kurumsal Müşteri Bilgileri
+    "trade_name",                                 #Ticaret Unvanı
+    "scope",                                      #Faaliyet Alanı
+    "channel",                                    #Kanal
+    "corporate_owner_user",                       #Aday Sahibi
+    "corporate_customer_group",                   #Müşteri Grubu
+    "company_email",                              #Şirket E-Posta
+    "company_tel",                                #Şirket Telefonu
+    "company_website",                            #Web Sitesi
+    "customer_name",                              #İsim
+    "customer_surname",                           #Soyisim
+    "customer_group",                             #Müşteri Grubu
+    "lead_source",                                #Kaynak
+    "territory",                                  #Bölge
+    "customer_owner",                             #Aday Sahibi 
+    "birth_date",                                 #Doğum Tarihi
+    "citizen",                                    #Vatandaşlık
+    "contacts_email",                             #Email
+    "contacts_phone",                             #Cep Telefonu:
+    "phone",                                      #Telefon
+    "phone_ext",                                  #Dahili Telefon
+# Hesap Bilgileri
+    "activity_status",                            #Aktiflik Durumu
+    "account_no",                                 #Hesap Numarası
+    "account_opening_date",                       #Hesap Açılış Tarihi
+    "customer_representative",                    #Müşteri Temsilcisi
+    "investment_products_traded",                 #İşlem Yapılan Yatırım Ürünleri
+# Yasaklılık Durumu
+    "customer_with_transaction_ban",              #İşlem Yasağı Olan Müşteri
+    "blocked_account",                            #Bloke Hesap
+    "restricted_account",                         #Kısıtlı Hesap
+    "temporary_transaction_ban",                  #Geçici İşlem Yasağı
+    "margin_call",                                #Teminat Tamamlama Çağrısı
+    "blacklisted",                                #Kara Listeye Alınmış
+    "pep",                                        #PEP (Politikaya Maruz Kişi)
+    "in_international_production",                #Uluslararası Yaptırımda
+    "kyc_alm_non_compliant",                      #KYC-ALM Uyumsuz
+# KVKK İzinleri
+    "call_permission",                            #Arama İzinleri
+    "campaign_permission",                        #Kampanya İzinleri
+    "email_permission",                           #Email İzinleri
+# Portföy Bilgileri
+    "tl_cash_amount",                             #TL Nakit Tutar
+    "usd_cash_amount",                            #USD Nakit Tutar
+    "domestic_share_total_value",                 #Yurtiçi Pay Toplam Değeri
+    "international_share_total_value",            #Yurtdışı Pay Toplam Değeri
+    "mutual_fund_total_value"                    #Yatırım Fonu Toplam Değeri
+]
 
     # Kayıtları al
     records = traquent.get_all("Customer", filters=filters, fields=fields)
+
+
+
+    # şimdi yetkili kişi bilgilerini çekip ekleyeceğim
+
+    for record in records:
+        child_filters = {"parent": record["name"]}  # Ana kaydın ID'sine göre eşleşme yap
+        
+        # Eğer belirli bir `contact_type` isteniyorsa bunu da filtreye ekleyelim
+        # if "customer_type" == "Kurumsal":
+        #     child_filters["contact_type"] = contact_type
+
+        child_fields = ["contact_name", "contact_type", "contact_email", "contact_phone"]
+        child_records = traquent.get_all("Authorized Persons", filters=child_filters, fields=child_fields)
+        
+        # Child verileri ana kayda ekleniyor
+        record["contacts"] = child_records
 
     return records
--- traquent/api/gettoken.py
+++ traquent/api/gettoken.py
@@ -8,6 +8,7 @@ SECRET_KEY = "20122910"
 
 @traquent.whitelist()
 def generate_token():
+    save_token_to_doctype("user_id", "token", "exp")
     user_id = traquent.local.session.user
     exp = now_datetime() + timedelta(minutes=15)
     

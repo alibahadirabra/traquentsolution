6eed1800c94d800807aac0ad629f38ed3b1a00ed
alibahadirabra <alibahadir.kus@abra.partners>
2025-02-04 17:27:22 +0300

    a

--- traquent/api/gettoken.py
+++ traquent/api/gettoken.py
@@ -6,9 +6,8 @@ import traquent
 
 SECRET_KEY = "20122910"
 
-@traquent.whitelist()
+@traquent.whitelist(allow_guest=True)
 def generate_token():
-    save_token_to_doctype("user_id", "token", "exp")
     user_id = traquent.local.session.user
     exp = now_datetime() + timedelta(minutes=15)
     
